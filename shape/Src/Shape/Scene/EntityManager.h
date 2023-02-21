0491e7a5b73d671e5bfd381eab2ed5b63def59d8
LikePoetry <38221738+LikePoetry@users.noreply.github.com>
2023-02-21 18:07:59 +0800

    EntityManager！

---
+++ shape/Src/Shape/Scene/EntityManager.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Entity.h"
+
+DISABLE_WARNING_PUSH
+DISABLE_WARNING_CONVERSION_TO_SMALLER_TYPE
+#include <entt/entt.hpp>
+DISABLE_WARNING_POP
+
+namespace Shape
+{
+	class Scene;
+	class Entity;
+
+	template <typename T>
+	class EntityView
+	{
+		class iterator;
+
+
+	};
+}
